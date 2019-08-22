b0a9c2ac29bedaad23dd94e9e772a4a8fac42a8d
Nirali <nirali420@gmail.com>
2019-08-22 20:31:06 +0000

    adding challange isOdd

---
+++ odd.js
@@ -0,0 +1,8 @@
+  var isOdd = function(num) {
+      if (num % 2 === 0)
+          console.log("false");
+      else
+          console.log("true");
+  }
+  console.log(isOdd(3));
+  console.log(isOdd(8));
